526e363e8fe1e07bf78b114dd157b42b7ca2eb85
atozcartoonist <72329036+atozcartoonist@users.noreply.github.com>
2023-06-24 23:08:52 +0530

    Update ffmpeg_helper.py

--- helpers/ffmpeg_helper.py
+++ helpers/ffmpeg_helper.py
@@ -95,7 +95,7 @@ async def MergeSub(filePath: str, subPath: str, user_id):
             subTrack += 1
     muxcmd.append(f"-metadata:s:s:{subTrack}")
     subTrack += 1
-    subTitle = f"Track {subTrack} - English (AtoZCartoonist)"
+    subTitle = f"Track {subTrack} AtoZCartoonist (eng)"
     muxcmd.append(f"title={subTitle}")
     muxcmd.append("-c:v")
     muxcmd.append("copy")
